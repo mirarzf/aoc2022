6a324dc1182946e375a8212ee83a4e5036281844
mirar <mira.razaf@gmail.com>
2022-12-11 21:30:13 +0900

    Added day 07 python solution (complete)

--- pysolutions/day07/day07.py
+++ pysolutions/day07/day07.py
@@ -20,19 +20,15 @@ def solve(inputfile:str, puzzlepart:int):
             if linesplit[2] != "..": 
                 # Explore subdirectory 
                 currPath.append(linesplit[2])
-                # print("on descend", currPath[-1])
             else: 
                 # Explore parent directory 
                 currPath.pop()
-                # print("on remonte", currPath[-1])
-            print(currPath)
 
         elif line[0] != '$': # ls output    
             if linesplit[0] == "dir": 
                 # Add subdir to list of directories 
                 subdirpath = pathListToString(currPath + [linesplit[1]])
                 dirSizes[subdirpath] = 0
-                print("yo", subdirpath, currPath[-1])
                 
             else:  
                 # Add file to current directory 
@@ -40,16 +36,19 @@ def solve(inputfile:str, puzzlepart:int):
                 for i in range(len(currPath)): 
                     dirkey = pathListToString(currPath[:i+1])
                     dirSizes[dirkey] += int(linesplit[0])
-                    print(dirkey, dirSizes[dirkey])
-                print("meh", currPath[:1])
         
-
-    print("solution part 1")
     if puzzlepart == 1: 
         for dir in dirSizes.keys(): 
             size = dirSizes[dir]
             if size <= 100000: 
-                # print(dir, size)
                 score += size
-        
+    
+    if puzzlepart == 2: 
+        outerSize = dirSizes["//"]
+        minimumSpace = outerSize - 40000000
+        minimalDirSpace = outerSize
+        for dir in dirSizes.keys(): 
+            if dirSizes[dir] < minimalDirSpace and dirSizes[dir] > minimumSpace:  
+                minimalDirSpace = dirSizes[dir]
+        return minimalDirSpace
     return score 
