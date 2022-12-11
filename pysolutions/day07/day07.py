42a5c86f440dd829cb2cc4f22db3171d14423414
mirar <mira.razaf@gmail.com>
2022-12-11 20:44:23 +0900

    Updated day 07 python solution (not working)

--- pysolutions/day07/day07.py
+++ pysolutions/day07/day07.py
@@ -3,8 +3,8 @@ def solve(inputfile:str, puzzlepart:int):
     lines = f.readlines() 
     score = 0 
     
-    currDir = "/"
-    parentsDir = {"/": ["", 0]}
+    dirSizes = {"/": 0}
+    currPath = ["/"]
 
     for line in lines:  
         linesplit = line.split() 
@@ -12,33 +12,29 @@ def solve(inputfile:str, puzzlepart:int):
             # Change directory 
             if linesplit[2] != "..": 
                 # Explore subdirectory 
-                currDir = linesplit[2]
-                print("on descend", currDir)
+                currPath.append(linesplit[2])
+                # print("on descend", currPath[-1])
             else: 
                 # Explore parent directory 
-                currDir = parentsDir[currDir][0]
-                print("on remonte", currDir)
+                currPath.pop()
+                # print("on remonte", currPath[-1])
 
         elif line[0] != '$': # ls output    
             if linesplit[0] == "dir": 
                 # Add subdir to list of directories 
-                parentsDir[linesplit[1]] = [currDir, 0]
-                print("yo", linesplit[1], currDir)
+                dirSizes[linesplit[1]] = 0
+                # print("yo", linesplit[1], currPath[-1])
                 
             else:  
                 # Add file to current directory 
                 # ie update size of all directories in the path of the file 
-                tempDir = currDir
-                while tempDir != "": 
-                    parentsDir[tempDir][1] += int(linesplit[0])
-                    print("yay", tempDir, parentsDir[tempDir])
-                    tempDir = parentsDir[tempDir][0]
-                    if tempDir != "": 
-                        print("yay2", tempDir, parentsDir[tempDir])
+                for dir in currPath: 
+                    dirSizes[dir] += int(linesplit[0])
+                    # print(dir, dirSizes[dir])
 
     if puzzlepart == 1: 
-        for dir in parentsDir.keys(): 
-            size = parentsDir[dir][1]
+        for dir in dirSizes.keys(): 
+            size = dirSizes[dir]
             if size <= 100000: 
                 print(dir, size)
                 score += size
